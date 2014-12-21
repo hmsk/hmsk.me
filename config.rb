68011d8360b848cdecd9b1cc2e94d9a9bf04c508
hmsk <k.hamasaki@gmail.com>
2014-12-21 18:16:12 +0900

    Renew layout

--- config.rb
+++ config.rb
@@ -1,6 +1,15 @@
+require 'digest/md5'
+
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
+
+helpers do
+  def gravatar_for(email)
+    hash = Digest::MD5.hexdigest(email.chomp.downcase)
+    "http://www.gravatar.com/avatar/#{hash}"
+  end
+end
 
 # Build-specific configuration
 configure :build do
@@ -14,4 +23,3 @@ ready do
   sprockets.append_path File.join root, 'bower_components'
   sprockets.append_path File.join root, 'bower_components', 'font-awesome', 'fonts' # Define directly for including woff to sprockets
 end
-
