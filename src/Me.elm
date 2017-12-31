4e914ea18a0fbf55ebb485e2acae53c1b8059407
hmsk <k.hamasaki@gmail.com>
2017-12-31 02:10:49 -0800

    Rotate accounts with j/k key 💃

--- src/Me.elm
+++ src/Me.elm
@@ -3,20 +3,26 @@ module Me exposing (..)
 import Platform
 import Html exposing (Html, div, text, strong, program, p, a, ul, li, i, img)
 import Html.Attributes exposing (attribute, style)
+import Keyboard exposing (..)
+import Char exposing (fromCode)
+
 import MetaData exposing (..)
 
 -- MODEL
 type alias Model =
     { accounts: List(Account)
+    , rotation: Int
     }
 
 init : ( Model, Cmd Msg )
 init =
-    (Model MetaData.accounts, Cmd.none)
+    ({ accounts = MetaData.accounts
+    ,  rotation = 0
+    }, Cmd.none)
 
 -- MESSAGES
 type Msg
-    = NoOp
+    = Presses Char
 
 -- VIEW
 view : Model -> Html Msg
@@ -25,20 +31,26 @@ view model =
     [ img [ attribute "src" MetaData.iconUrl ] []
     , p [ attribute "class" "name" ] [ text MetaData.myName ]
     , p [] (profilesLinksHtml MetaData.profiles)
-    , ul [] (circleAccountList model.accounts)
+    , ul [] (circleAccountList model.accounts model.rotation)
     ]
 
 -- UPDATE
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        Presses code ->
+            case code of
+                'j' ->
+                    ({ model | rotation = model.rotation + 1 }, Cmd.none)
+                'k' ->
+                    ({ model | rotation = model.rotation - 1 }, Cmd.none)
+                _ ->
+                    (model, Cmd.none)
 
 -- SUBSCRIPTIONS
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Keyboard.presses (\code -> Presses (fromCode code))
 
 -- MAIN
 main : Program Never Model Msg
@@ -59,13 +71,13 @@ profileLink (_, icon, url) =
     a [ attribute "href" url, attribute "target" "_blank"]
       [ i [ attribute "class" icon ] [] ]
 
-circleAccountList : List(Account) -> List(Html msg)
-circleAccountList (accounts) =
-    List.indexedMap (\i account -> circleAccountHtml account (List.length accounts) i) accounts
+circleAccountList : List(Account) -> Int -> List(Html msg)
+circleAccountList accounts rotation =
+    List.indexedMap (\i account -> circleAccountHtml account (List.length accounts) i rotation) accounts
 
-circleAccountHtml : Account -> Int -> Int -> Html msg
-circleAccountHtml (name, icon, url) len num =
-    li [circularStyle len num] [
+circleAccountHtml : Account -> Int -> Int -> Int -> Html msg
+circleAccountHtml (name, icon, url) len num rotation =
+    li [circularStyle len num rotation] [
         a [ attribute "class" "button"
         , attribute "href" url
         , attribute "target" "_blank"
@@ -73,11 +85,13 @@ circleAccountHtml (name, icon, url) len num =
         [ i [attribute "class" icon] [] ]
     ]
 
-circularStyle : Int -> Int -> Html.Attribute msg
-circularStyle len num =
+circularStyle : Int -> Int -> Int -> Html.Attribute msg
+circularStyle len num rotation =
     let
-        deg = 360 / toFloat(len) * toFloat(num)
+        one = 360 / toFloat(len)
+        deg = one * toFloat(num) + one * toFloat(rotation)
     in
         style
             [ ("transform", "rotate(" ++ toString deg ++ "deg) translateY(-160px) rotate(" ++ toString (deg * -1) ++ "deg)")
+            , ("transition", "0.8s ease-in-out")
             ]
