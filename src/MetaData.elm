688953b2b177e04f79ad0763e94ef7e85b70c9d5
hmsk <k.hamasaki@gmail.com>
2019-10-01 01:41:41 -0700

    Refurbish links

--- src/MetaData.elm
+++ src/MetaData.elm
@@ -17,25 +17,25 @@ iconUrl =
 
 accounts : List Account
 accounts =
-    [ ( "Blog", "fas fa-edit", "https://hmsk.hatenablog.com" )
-    , ( "Photo", "fas fa-camera-retro", "http://pic.hmsk.me" )
-    , ( "Essay", "far fa-sticky-note", "https://hmsk.hatenablog.jp" )
-    , ( "GitHub", "fab fa-github", "https://github.com/hmsk" )
+    [ ( "LinkedIn", "fab fa-linkedin-in", "https://www.linkedin.com/in/hmsk" )
+    , ( "Give a coffee", "far fa-coffee-togo", "https://www.buymeacoffee.com/hmsk" )
+    , ( "Blog (en)", "fab fa-medium-m", "https://medium.com/haiiro-io/tagged/code" )
+    , ( "Blog (ja)", "far fa-edit", "https://hmsk.hatenablog.com" )
+    , ( "Essay (ja)", "far fa-pen-nib", "https://hmsk.hatenablog.jp" )
+    , ( "GitHub", "fab fa-github-alt", "https://github.com/hmsk" )
     , ( "Instagram", "fab fa-instagram", "https://instagram.com/hmsk" )
-    , ( "Flickr", "fab fa-flickr", "https://flickr.com/photos/hmsk/" )
+    , ( "Twitch", "fab fa-twitch", "https://www.twitch.tv/hmsk" )
     , ( "500px", "fab fa-500px", "https://500px.com/hmsk" )
     , ( "Twitter", "fab fa-twitter", "https://twitter.com/hmsk" )
     , ( "Facebook", "fab fa-facebook", "https://facebook.com/hamachang" )
-    , ( "Tumblr", "fab fa-tumblr-square", "https://hmsk.tumblr.com/" )
-    , ( "Fitbit", "fas fa-heartbeat", "https://www.fitbit.com/user/243MYQ" )
+    , ( "Fitbit", "far fa-running", "https://www.fitbit.com/user/243MYQ" )
     , ( "AngelList", "fab fa-angellist", "https://angel.co/hmsk" )
-    , ( "LinkedIn", "fab fa-linkedin", "https://www.linkedin.com/in/hmsk" )
-    , ( "Wishlist", "fas fa-gift", "https://www.amazon.co.jp/registry/wishlist/1U1J17EZM8CP1" )
+    , ( "Wish List", "far fa-gifts", "https://www.amazon.com/hz/wishlist/ls/273IUDAQJR4QQ" )
     ]
 
 
 profiles : List Account
 profiles =
-    [ ( "key", "fas fa-key", "https://keybase.io/hmsk/key.asc" )
-    , ( "partner", "fas fa-heart", "https://haiji.co" )
+    [ ( "key", "fad fa-fingerprint", "https://keybase.io/hmsk/key.asc" )
+    , ( "partner", "fad fa-rings-wedding", "https://namika.hmsk.co" )
     ]
